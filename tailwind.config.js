3e2ed286be747a33e91f6c047bdda97d5f457804
Jainana0306 <Jainana0306@gmail.com>
2023-11-10 16:17:02 +0700

    Navbar update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        '360': '360px', // Add a custom minimum width class
+      },
+    },
   },
   plugins: [],
 });
