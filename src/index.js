792105131764ed90e900460bceddc430e9bd10ac
punnuund <banthita.pun@gmail.com>
2023-11-11 00:33:13 +0700

    รวมด้านบนด้านล่าง

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import CreateAccount from './Pages/CreateAccount';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CreateAccount />
   </React.StrictMode>
 );
 
