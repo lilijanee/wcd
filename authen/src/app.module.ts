c44a453d7365155fb8da365d261ab40670bdf233
EgapNimda <6434449023@student.chula.ac.th>
2023-11-12 01:25:18 +0700

    Add user module
    Minacious Metamorphosis

--- authen/src/app.module.ts
+++ authen/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [LoginModule, AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
