f39bafb80869db666f4cebb8f6f26c15d444f70e
EgapNimda <6434449023@student.chula.ac.th>
2023-11-12 01:15:04 +0700

    Add authen module
    The Start of the End

--- authen/src/app.module.ts
+++ authen/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
