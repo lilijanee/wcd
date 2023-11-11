89fe8c6e6d6967bdd8f90df64caacd77c7a20512
CoventinaLachesis <81896578+CoventinaLachesis@users.noreply.github.com>
2023-11-11 22:00:24 +0700

    fix some shit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar";
 import CreateAccount from "./Pages/CreateAccount";
-import { MaterialTailwindProvider } from "@material-tailwind/react";
-import { GoogleMapsProvider } from "@ubilabs/google-maps-react-hooks";
+//import { MaterialTailwindProvider } from "@material-tailwind/react";
+//import { GoogleMapsProvider } from "@ubilabs/google-maps-react-hooks";
 export default function App() {
   
   return (
     <div className="App">
-      <Router>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<CreateAccount />} />
           <Route path="/signup" element={<CreateAccount />} />
+          <Route path="/Navbar" element={<Navbar />} />
         </Routes>
-      </Router>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import CreateAccount from './Pages/CreateAccount';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom' 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <CreateAccount />
-  </React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App/>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
