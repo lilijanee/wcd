b1fd03c9eb347b5e3a396b322475b8b0374f4b71
punnuund <banthita.pun@gmail.com>
2023-11-11 18:09:26 +0700

    update

--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -1,15 +1,21 @@
 import './CreateAccount.css';
-import bg from '../image/background.png';
 import lg2 from '../image/logo2.png';
 import bg from '../image/background.png'; // import the new background image
 
 export default function CreateAccount() {
   return (
-    <div style={{backgroundImage: `url(${bg})`}}> {/* set the new background image */}
+    <div > {/* set the new background image */}
+     
       <h1> Create an account</h1>
-      <h2>Already have an account? <a href="url">Sign in</a></h2>
-      <h3>Username</h3>
+      <h2>Already have an account?  
+        <a 
+        href="https://www.google.com/"
+        title="   Sign in">
+          Sign in</a>
+      </h2>
+  
       <form action='/google.com'>
+        <h3>Username</h3>
         <input type="text" id="Username" name="Username"></input>
         <h3>Password</h3>
         <input type="text" id="Password" name="Password"></input>
@@ -17,7 +23,7 @@ export default function CreateAccount() {
         <input type="text" id="ConfirmedPassword" ></input>
         <h3>PhoneNumber</h3>
         <input type="password" id="PhoneNumber" name="PhoneNumber"></input>
-        <input type="Continue" value="Continue"></input>
+        <button  id="Con">Continue</button>
       </form>
     </div>
   )
