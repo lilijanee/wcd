f71f0eacc5355a43abc3c4c4576afddab058cd46
EgapNimda <6434449023@student.chula.ac.th>
2023-11-12 22:45:49 +0700

    Copy code from an Indian youtuber
    Looming Demise

--- authen/src/app.module.ts
+++ authen/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from "./user.entity"
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'rootroot',
+      database: 'wcd',
+      entities: [User],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
